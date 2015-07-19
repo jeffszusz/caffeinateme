df37fb0c00db2cb70fe12c37dc7a0147a27638de
Jeffrey Szusz <jeff.szusz@gmail.com>
2015-07-19 15:30:10 -0400

    format the results to be human readable

--- needcoffee.py
+++ needcoffee.py
@@ -1,16 +1,23 @@
 #!/usr/bin/python
 
+
 def caffeinate(volumeOfWater=10):
     # Step 1:
     # Find the weight in grams of coffee needed for a given volume of water
 
     # 1 fl. oz of water weighs approx 28 grams
-    weightOfWater = volumeOfWater * 28 # grams
+    weightOfWater = volumeOfWater * 28  # grams
 
     # A weight ratio of 1:16 coffee to water makes a perfect cup
-    weightOfCoffee = weightOfWater / 16.0 # grams
+    weightOfCoffee = weightOfWater / 16.0  # grams
 
-    return weightOfCoffee
+    return formatResults(volumeOfWater, weightOfCoffee)
+
+
+def formatResults(waterVolume, coffeeWeight):
+    message = "For {} fl. oz of water, grind {} grams of coffee beans."
+    message = message.format(waterVolume, coffeeWeight)
+    return message
 
 print(caffeinate())
 
@@ -18,4 +25,3 @@ print(caffeinate())
 # Make the program accept values in the terminal and output the results
 #   if there are command-line arguments, use the volume value(s) provided
 #   if there are no arguments, use defaults of 8, 10 and 12 fl. oz
-
