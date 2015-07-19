613ad6cdf624105add4b5bd42df7fdb821a61211
Jeffrey Szusz <jeff.szusz@gmail.com>
2015-07-19 15:03:09 -0400

    Requirements added as comments

--- needcoffee.py
+++ needcoffee.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python
+
+    # Step 1:
+    # Find the weight in grams of coffee needed for a given volume of water
+    #   1 fl. oz of water weighs approx 28 grams
+    #   A weight ratio of 1:16 coffee to water makes a perfect cup
+
+    # Step 2:
+    # Make the program accept values in the terminal and output the results
+    #   if there are command-line arguments, use the volume value(s) provided
+    #   if there are no arguments, use defaults of 8, 10 and 12 fl. oz
+
