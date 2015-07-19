961b7d565609eafecd1d0d0a151a2229233ba7be
Jeffrey Szusz <jeff.szusz@gmail.com>
2015-07-19 15:09:05 -0400

    calculate weight ratio of coffee to water volume given

--- needcoffee.py
+++ needcoffee.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python
 
-    # Step 1:
-    # Find the weight in grams of coffee needed for a given volume of water
-    #   1 fl. oz of water weighs approx 28 grams
-    #   A weight ratio of 1:16 coffee to water makes a perfect cup
+# Step 1:
+# Find the weight in grams of coffee needed for a given volume of water
+volumeOfWater = 10
 
-    # Step 2:
-    # Make the program accept values in the terminal and output the results
-    #   if there are command-line arguments, use the volume value(s) provided
-    #   if there are no arguments, use defaults of 8, 10 and 12 fl. oz
+# 1 fl. oz of water weighs approx 28 grams
+weightOfWater = volumeOfWater * 28 # grams
+
+# A weight ratio of 1:16 coffee to water makes a perfect cup
+weightOfCoffee = weightOfWater / 16.0 # grams
+
+print(weightOfCoffee)
+
+# Step 2:
+# Make the program accept values in the terminal and output the results
+#   if there are command-line arguments, use the volume value(s) provided
+#   if there are no arguments, use defaults of 8, 10 and 12 fl. oz
 
